228ccb8312cc3f8e210aadb9aba6c796b21d4795
Mauran Muthiah <mauran@users.noreply.github.com>
2018-12-21 08:26:09 +0100

    Update SlackVoteController.php

--- src/Controller/SlackVoteController.php
+++ src/Controller/SlackVoteController.php
@@ -35,6 +35,7 @@ class SlackVoteController extends AbstractController
         }
         $question = $question[0] . '?';
         $answers = str_replace($question, '', $text);
+        $answers = str_replace('”", '"', $answer);
         preg_match_all('/"(?:\\\\.|[^\\\\"])*"|\S+/', $answers, $answers);
         $answers = $answers[0];
 
