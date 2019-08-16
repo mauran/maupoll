b950ee76fa581525dee791e812f06071855f69c8
Mauran <mauran@maurans-mbp.lan>
2019-08-16 16:34:44 +0200

    Added questionController

--- src/Controller/Adminstration/QuestionController.php
+++ src/Controller/Adminstration/QuestionController.php
@@ -2,9 +2,26 @@
 
 
 namespace App\Controller\Adminstration;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-
-class QuestionController
+/**
+ * Class QuestionController
+ * @package App\Controller\Adminstration
+ * @Route("/admin/questions")
+ */
+class QuestionController extends AbstractController
 {
 
+    /**
+     * @Route("/")
+     */
+    public function index(Request $request, EntityManagerInterface $em) {
+
+
+        return new Response('HELLO');
+    }
 }
